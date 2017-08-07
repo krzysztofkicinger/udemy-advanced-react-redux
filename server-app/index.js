bcc22a1cd95ef731fcbe1be331df940d1d0659b6
Krzysztof Kicinger <krzysztofkicinger@gmail.com>
2017-08-07 13:07:39 +0200

    [server-app] Server setup

--- server-app/index.js
+++ server-app/index.js
@@ -1,1 +1,17 @@
 // Main starting point of the application
+const express = require('express');
+const http = require('http');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
+
+// App Setup
+const app = express();
+
+
+// Server Setup
+const port = process.env.PORT || 9300;
+
+const server = http.createServer(app);
+server.listen(port);
+
+console.log('Server listening on port: ', port);
