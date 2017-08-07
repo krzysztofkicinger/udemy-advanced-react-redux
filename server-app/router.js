44bc50e6324c5225fd58301eafa301666cee2e00
Krzysztof Kicinger <krzysztofkicinger@gmail.com>
2017-08-07 15:36:16 +0200

    [server-app] Chaining a '/' GET request with Passport Authorization based on JWT

--- server-app/router.js
+++ server-app/router.js
@@ -1,6 +1,18 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+const requireAuth = passport.authenticate('jwt', {
+    session: false
+});
 
 module.exports = function(app) {
+
+    app.get('/', requireAuth, function(request, response) {
+        response.send({
+            hi: 'there'
+        });
+    });
 
     // next - for error handling
     app.post('/', Authentication.signup);
