9f714e862734b88bf35627eb8d2b12791864dc93
Krzysztof Kicinger <krzysztofkicinger@gmail.com>
2017-08-17 09:10:33 +0200

    [client-app] Header actions (sign in, sign out, sign up)

--- client-app/src/components/header.jsx
+++ client-app/src/components/header.jsx
@@ -1,38 +1,47 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Link } from 'react-router';
 
 class Header extends Component {
-
-    renderSignInListItem = () => {
-        return this.renderListItem('Sign In');
-    };
-
-    renderSignOutListItem = () => {
-        return this.renderListItem('Sign Out');
-    };
-
-    renderListItem = (label) =>
-        <li className="nav-item">
-            { label }
-        </li>;
 
     render = () => {
         console.log(`Authenticated ${this.props.authenticated}`);
         return (
             <nav className="navbar navbar-light">
                 <ul className="nav navbar-nav">
-                    { this.props.authenticated ? this.renderSignOutListItem() : this.renderSignInListItem() }
+                    <Link to="/" className="navbar-brand">Redux Auth</Link>
+                    { this.props.authenticated ? this.renderSignOutListItem() : this.renderNotAuthorizedUserActions() }
                 </ul>
             </nav>
         )
-    }
+    };
+
+    renderSignInListItem = () => {
+        return this.renderListItem('Sign In', '/signin');
+    };
+
+    renderNotAuthorizedUserActions = () => [
+        this.renderSignInListItem(),
+        this.renderSignUpListItem()
+    ];
+
+    renderSignOutListItem = () => {
+        return this.renderListItem('Sign Out', '/signout');
+    };
+
+    renderSignUpListItem = () => {
+        return this.renderListItem('Sign Up', '/signup');
+    };
+
+    renderListItem = (label, path) =>
+        <li className="nav-item" key={path}>
+            <Link className="nav-link" to={path}>{label}</Link>
+        </li>;
 
 }
 
-const mapStateToProps = (state) => {
-    console.log(state);
-    return {
-        authenticated: state.auth.authenticated
-}};
+const mapStateToProps = ({ auth }) => ({
+    authenticated: auth.authenticated
+});
 
 export default connect(mapStateToProps)(Header);
